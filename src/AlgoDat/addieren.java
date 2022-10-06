49a0c9d878631e09db5a01fb71b0c6fb85c2616e
mokokla <moritz.schneider33@gmail.com>
2022-10-06 15:53:58 +0200

    initial- commit

---
+++ src/AlgoDat/addieren.java
@@ -0,0 +1,11 @@
+package AlgoDat;
+import java.util.Scanner;
+
+public class addieren {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("erste Zahl: ");
+        int Zahl1 = scanner.nextInt();
+        int Zahl2 = scanner.nextInt();
+    }
+}
