0ea921055b31b7befa7d8d606a8df235a22caaca
mokokla <moritz.schneider33@gmail.com>
2022-10-06 16:32:55 +0200

    second commit

--- src/AlgoDat/addieren.java
+++ src/AlgoDat/addieren.java
@@ -5,8 +5,21 @@ public class addieren {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("erste Zahl: ");
-        int Zahl1 = scanner.nextInt();
+        String Zahl1 = scanner.nextLine();
         System.out.println("zweite Zahl: ");
-        int Zahl2 = scanner.nextInt();
+        String Zahl2 = scanner.nextLine();
+
+        char[] array1 = new char[Zahl1.length()];
+        char[] array2 = new char[Zahl2.length()];
+
+        for (int i = 0; i < Zahl1.length(); i++) {
+            array1[i] = Zahl1.charAt(i);
+        }
+        for (int i = 0; i < Zahl2.length(); i++) {
+            array2[i] = Zahl2.charAt(i);
+        }
+
+        //int number = Integer.parseInt(str);
+
     }
 }
