27a5ffbe4a43845028ab0afab3b6df3663362257
mokokla <moritz.schneider33@gmail.com>
2022-10-06 15:57:16 +0200

    first commit

--- src/AlgoDat/addieren.java
+++ src/AlgoDat/addieren.java
@@ -6,6 +6,7 @@ public class addieren {
         Scanner scanner = new Scanner(System.in);
         System.out.println("erste Zahl: ");
         int Zahl1 = scanner.nextInt();
+        System.out.println("zweite Zahl: ");
         int Zahl2 = scanner.nextInt();
     }
 }
