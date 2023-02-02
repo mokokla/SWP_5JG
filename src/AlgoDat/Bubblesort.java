50000f1f51953651617307381cbaa8f9f8f0f610
mokokla <94524895+mokokla@users.noreply.github.com>
2023-02-02 11:45:07 +0100

    Algodat part 2

---
+++ src/AlgoDat/Bubblesort.java
@@ -0,0 +1,16 @@
+package AlgoDat;
+
+public class Bubblesort {
+    public static void bubbleSort(int[] arr) {
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ src/AlgoDat/selectionSort.java
@@ -0,0 +1,24 @@
+package AlgoDat;
+
+public class selectionSort {
+    public static void main(String[] args) {
+            int[] arr = new int[0];
+            int n = arr.length;
+
+
+            for (int i = 0; i < n - 1; i++) {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[minIndex]) {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+    }
+
